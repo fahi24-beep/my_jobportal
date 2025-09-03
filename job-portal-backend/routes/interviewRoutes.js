a1124eadfe48f3c99823798002f8768319fc0af0
fahi24-beep <fahmida24102000@gmail.com>
2025-09-03 18:23:33 +0600

    Delete job-portal-backend/routes directory

--- job-portal-backend/routes/interviewRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendInterviewInvitation } from "../controllers/interviewController.js";
-
-const router = express.Router();
-
-router.post("/send", sendInterviewInvitation);
-
-export default router;
--- job-portal-backend/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submitReview } from "../controllers/reviewController.js";
-
-const router = express.Router();
-
-router.post("/", submitReview);
-
-export default router;
